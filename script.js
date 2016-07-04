e7f1dea0470c624302c13f2a04860631c1f3a31d
Jomar Santos <jomaroliversantos@gmail.com>
2016-07-04 14:28:19 -0700

    Opening of workspaces working

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.getElementById("saveWorkspace").onclick = function() {
     tabs: workspaceTabs
   }
 
-  workspaces.push(newWorkspace);
+  workspaces.unshift(newWorkspace);
   chrome.storage.sync.set({workspaces: workspaces});
 
   showNewWorkspace(workspaceName);
@@ -77,23 +77,74 @@ document.getElementById("saveWorkspace").onclick = function() {
   resetForm();
 }
 
-// Add New Workspace to View
-function showNewWorkspace(workspaceNameText) {
-  var workspacesDiv = document.getElementById("workspaces");
-  var workspaceBtn = document.createElement("button");
-  var workspaceName = document.createElement("h3");
-  workspaceName.className = "workspaceName";
-  workspaceName.innerHTML = workspaceNameText.toUpperCase();
+// Load Workspaces on Startup
+function loadWorkspaces() {
+  var i, clone, workspace;
 
-  workspaceBtn = document.createElement("button");
-  workspaceBtn.className = 'workspace';
-  workspaceBtn.id = workspaceNameText;
-  workspaceBtn.onclick = function() {
-    openWorkspace(workspaceNameText);
-  }
-  workspaceBtn.appendChild(workspaceName);
-  workspacesDiv.appendChild(workspaceBtn);
+  chrome.storage.sync.get({workspaces: []}, function(data) {
+    workspaces = data.workspaces;
+
+    for (i = 0; i < workspaces.length; i++) {
+      createWorkspaceButton(workspaces[i].name);
+    }
+  });
 }
+
+// Returns the Button of a Workspace
+function createWorkspaceButton(workspaceName) {
+  var workspacesDiv = document.getElementById("workspaces");
+  var workspaceTemplate = document.querySelector("#workspaceTemplate");
+  var workspace = document.importNode(workspaceTemplate.content, true).querySelector("div");
+  var workspaceNameText;
+
+  workspace.onclick = function() {
+    openWorkspace(workspaceName);
+  }
+  
+  workspace.id = workspaceName;
+  workspaceNameText = workspaceName.toUpperCase();
+  workspace.querySelector("h3").innerHTML = workspaceNameText;
+  workspacesDiv.appendChild(workspace);
+}
+
+// Add New Workspace to View
+function showNewWorkspace(workspaceName) {
+  var workspacesDiv = document.getElementById("workspaces");
+  var firstWorkspace = document.getElementsByClassName("workspace")[0];
+  var workspace = createWorkspaceButton(workspaceName);
+  workspacesDiv.insertBefore(workspace, firstWorkspace);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Open Workspace Clicked
 function openWorkspace(workspaceName) {
@@ -102,6 +153,7 @@ function openWorkspace(workspaceName) {
   var workspace = {};
 
   workspace = getWorkspace(workspaceName);
+  console.log(workspace);
   tabs = workspace.tabs;
 
   for (i = 0; i < tabs.length; i++) {
@@ -116,32 +168,6 @@ function getWorkspace(workspaceName) {
       return workspaces[i];
     }
   }
-}
-
-// Load Workspaces on Startup
-function loadWorkspaces() {
-  var workspacesDiv = document.getElementById("workspaces");
-  var i, workspaceBtn, workspaceNameText, workspaceName;
-
-  chrome.storage.sync.get({workspaces: []}, function(data) {
-    workspaces = data.workspaces;
-
-    for (i = 0; i < workspaces.length; i++) {
-      workspaceNameText = workspaces[i].name;
-      var workspaceName = document.createElement("h3");
-      workspaceName.className = "workspaceName";
-      workspaceName.innerHTML = workspaceNameText.toUpperCase();
-
-      workspaceBtn = document.createElement("button");
-      workspaceBtn.className = 'workspace';
-      workspaceBtn.id = workspaceNameText;
-      workspaceBtn.onclick = function() {
-        openWorkspace(workspaceNameText);
-      }
-      workspaceBtn.appendChild(workspaceName);
-      workspacesDiv.appendChild(workspaceBtn);
-    }
-  });
 }
 
 function resetForm() {
@@ -182,106 +208,106 @@ function resetForm() {
 
 
 
-// Left Scroll
-document.getElementById("directionArrowWorkspaceLeft").onclick = function() {
-  var workspacesContainer = document.getElementById("workspaces");
-  var workspace = document.getElementsByClassName("workspace");
-  var diff = workspacesContainer.scrollLeft % 170;
-  workspacesContainer.scrollLeft = workspacesContainer.scrollLeft - diff - 510;
-}
+// // Left Scroll
+// document.getElementById("directionArrowWorkspaceLeft").onclick = function() {
+//   var workspacesContainer = document.getElementById("workspaces");
+//   var workspace = document.getElementsByClassName("workspace");
+//   var diff = workspacesContainer.scrollLeft % 170;
+//   workspacesContainer.scrollLeft = workspacesContainer.scrollLeft - diff - 510;
+// }
+//
+// // Right Scroll
+// document.getElementById("directionArrowWorkspaceRight").onclick = function() {
+//   var workspacesContainer = document.getElementById("workspaces");
+//   var workspace = document.getElementsByClassName("workspace");
+//   workspacesContainer.scrollLeft = workspacesContainer.scrollLeft + 680;
+// }
 
-// Right Scroll
-document.getElementById("directionArrowWorkspaceRight").onclick = function() {
-  var workspacesContainer = document.getElementById("workspaces");
-  var workspace = document.getElementsByClassName("workspace");
-  workspacesContainer.scrollLeft = workspacesContainer.scrollLeft + 680;
-}
+// // Toggle All Workspace View
+// document.getElementById("showAll").onclick = function() {
+//   var workspaces = document.getElementById("workspaces");
+//   var workspacesContainer = document.getElementById("workspacesContainer");
+//   var leftArrow = document.getElementById("directionArrowWorkspaceLeft");
+//   var rightArrow = document.getElementById("directionArrowWorkspaceRight");
+//   var backButton = document.getElementById("backButton");
+//   var showAllButton = document.getElementById("showAll");
+//
+//
+//   if (workspaces.style.height == 'auto') {
+//     workspaces.style.height = '150px';
+//     workspaces.style.overflow = 'scroll';
+//     workspaces.style.overflowY = 'hidden';
+//     workspaces.style.whiteSpace = 'nowrap';
+//     workspacesContainer.style.overflow = 'hidden';
+//     workspacesContainer.style.marginBottom = "0px";
+//     leftArrow.style.display = 'inline-block';
+//     rightArrow.style.display = 'inline-block';
+//     backButton.style.display = 'none';
+//     showAllButton.style.display = 'inline';
+//   } else {
+//     workspaces.style.height = 'auto';
+//     workspaces.style.overflow = 'visible';
+//     workspaces.style.overflowY = 'visible';
+//     workspaces.style.whiteSpace = 'normal';
+//     workspacesContainer.style.overflow = 'visible';
+//     workspaces.style.marginBottom = "80px";
+//     leftArrow.style.display = 'none';
+//     rightArrow.style.display = 'none';
+//     backButton.style.display = 'inline';
+//     showAllButton.style.display = 'none';
+//   }
+// }
+//
+// // Toggle All Workspace View
+// document.getElementById("backButton").onclick = function() {
+//   var workspaces = document.getElementById("workspaces");
+//   var workspacesContainer = document.getElementById("workspacesContainer");
+//   var leftArrow = document.getElementById("directionArrowWorkspaceLeft");
+//   var rightArrow = document.getElementById("directionArrowWorkspaceRight");
+//   var backButton = document.getElementById("backButton");
+//   var showAllButton = document.getElementById("showAll");
+//
+//   workspaces.style.height = '150px';
+//   workspaces.style.overflow = 'scroll';
+//   workspaces.style.overflowY = 'hidden';
+//   workspaces.style.whiteSpace = 'nowrap';
+//   workspacesContainer.style.overflow = 'hidden';
+//   leftArrow.style.display = 'inline-block';
+//   rightArrow.style.display = 'inline-block';
+//   backButton.style.display = 'none';
+//   showAllButton.style.display = 'inline';
+// }
+//
+// // Toggle New Workspace Dialogue
+// document.getElementById("addButton").onclick = function() {
+//   var container = document.getElementById("sidebar");
+//   var addButton = document.getElementById("addButton");
+//
+//   if (container.style.width == '300px') {
+//     container.style.width = '0px';
+//   } else {
+//     container.style.width = '300px';
+//   }
+// }
 
-// Toggle All Workspace View
-document.getElementById("showAll").onclick = function() {
-  var workspaces = document.getElementById("workspaces");
-  var workspacesContainer = document.getElementById("workspacesContainer");
-  var leftArrow = document.getElementById("directionArrowWorkspaceLeft");
-  var rightArrow = document.getElementById("directionArrowWorkspaceRight");
-  var backButton = document.getElementById("backButton");
-  var showAllButton = document.getElementById("showAll");
+// // Hide New Workspace Dialogue on X Click
+// document.getElementById("closeSidebar").onclick = closeDialogue();
+//
+// // Hide New Workspace Dialogue On Mouse Click Elsewhere
+// document.addEventListener('mouseup', function (e) {
+//   var container = document.getElementById("sidebar");
+//   var exitButton = document.getElementById("closeSidebar");
+//
+//   if (!container.contains(e.target) || e.target == exitButton) {
+//     closeDialogue();
+//   }
+// }.bind(this));
 
-
-  if (workspaces.style.height == 'auto') {
-    workspaces.style.height = '150px';
-    workspaces.style.overflow = 'scroll';
-    workspaces.style.overflowY = 'hidden';
-    workspaces.style.whiteSpace = 'nowrap';
-    workspacesContainer.style.overflow = 'hidden';
-    workspacesContainer.style.marginBottom = "0px";
-    leftArrow.style.display = 'inline-block';
-    rightArrow.style.display = 'inline-block';
-    backButton.style.display = 'none';
-    showAllButton.style.display = 'inline';
-  } else {
-    workspaces.style.height = 'auto';
-    workspaces.style.overflow = 'visible';
-    workspaces.style.overflowY = 'visible';
-    workspaces.style.whiteSpace = 'normal';
-    workspacesContainer.style.overflow = 'visible';
-    workspaces.style.marginBottom = "80px";
-    leftArrow.style.display = 'none';
-    rightArrow.style.display = 'none';
-    backButton.style.display = 'inline';
-    showAllButton.style.display = 'none';
-  }
-}
-
-// Toggle All Workspace View
-document.getElementById("backButton").onclick = function() {
-  var workspaces = document.getElementById("workspaces");
-  var workspacesContainer = document.getElementById("workspacesContainer");
-  var leftArrow = document.getElementById("directionArrowWorkspaceLeft");
-  var rightArrow = document.getElementById("directionArrowWorkspaceRight");
-  var backButton = document.getElementById("backButton");
-  var showAllButton = document.getElementById("showAll");
-
-  workspaces.style.height = '150px';
-  workspaces.style.overflow = 'scroll';
-  workspaces.style.overflowY = 'hidden';
-  workspaces.style.whiteSpace = 'nowrap';
-  workspacesContainer.style.overflow = 'hidden';
-  leftArrow.style.display = 'inline-block';
-  rightArrow.style.display = 'inline-block';
-  backButton.style.display = 'none';
-  showAllButton.style.display = 'inline';
-}
-
-// Toggle New Workspace Dialogue
-document.getElementById("addButton").onclick = function() {
-  var container = document.getElementById("sidebar");
-  var addButton = document.getElementById("addButton");
-
-  if (container.style.width == '300px') {
-    container.style.width = '0px';
-  } else {
-    container.style.width = '300px';
-  }
-}
-
-// Hide New Workspace Dialogue on X Click
-document.getElementById("closeSidebar").onclick = closeDialogue();
-
-// Hide New Workspace Dialogue On Mouse Click Elsewhere
-document.addEventListener('mouseup', function (e) {
-  var container = document.getElementById("sidebar");
-  var exitButton = document.getElementById("closeSidebar");
-
-  if (!container.contains(e.target) || e.target == exitButton) {
-    closeDialogue();
-  }
-}.bind(this));
-
-function closeDialogue() {
-  var container = document.getElementById("sidebar");
-  container.style.width = '0px';
-  addButton.style.display = 'inline';
-}
+// function closeDialogue() {
+//   var container = document.getElementById("sidebar");
+//   container.style.width = '0px';
+//   addButton.style.display = 'inline';
+// }
 
 
 
